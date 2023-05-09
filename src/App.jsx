fa6da53e609ccdafd41001d6ae8ef45c3fd43704
traducha <traducha@vayandata.com>
2023-05-09 12:36:50 +0200

    test(calendar): design

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  const events = [{ title: "test", start: new Date() }];
+  const events = [
+    {
+      title: "Ceci est un test",
+      start: new Date(),
+    },
+  ];
 
   function renderEventContent(eventInfo) {
     return (
-      <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-      </>
+      <Card sx={{ borderRadius: 2 }}>
+        <CardContent
+          sx={{
+            p: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          style={{ backgroundColor: "#364e68", color: "white" }}
+        >
+          <Typography color="white" gutterBottom variant="h6">
+            {eventInfo.timeText}
+          </Typography>
+          <Typography color="white" gutterBottom variant="h6">
+            {eventInfo.event.title}
+          </Typography>
+        </CardContent>
+      </Card>
     );
   }
 
   return (
-    <div>
+    <div className="App">
       <h1>Calendar</h1>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        weekends={false}
-        events={events}
-        eventContent={renderEventContent}
-      />
+      <div className="full-calendar">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          weekends={true}
+          events={events}
+          eventContent={renderEventContent}
+          dayMaxEvents={true}
+          // editable={true}
+          // selectable={true}
+          // selectMirror={true}
+        />
+      </div>
     </div>
   );
 }
