b69b0d8940d2e9adc23da78d4d0bc7949af6d2d9
traducha <traducha@vayandata.com>
2023-06-01 16:17:31 +0200

    fix(calendar): EventResize

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import PopupEvent from "./PopupEvent";
+import { CleaningServices } from "@mui/icons-material";
 
 const Calendar = () => {
   const [events, setEvents] = useState([]);
+  console.log("🚀 ~ file: Calendar.jsx:10 ~ Calendar ~ events:", events);
   const [openPopup, setOpenPopup] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const calendarRef = useRef(null);
@@ -16,11 +18,11 @@ const Calendar = () => {
     right: "dayGridMonth,timeGridWeek,timeGridDay",
   };
 
-  useEffect(() => {
-    const calendarApi = calendarRef.current.getApi();
-    calendarApi.removeAllEvents();
-    events.forEach((event) => calendarApi.addEvent(event));
-  }, [events]);
+  // useEffect(() => {
+  //   const calendarApi = calendarRef.current.getApi();
+  //   calendarApi.removeAllEvents();
+  //   events.forEach((event) => calendarApi.addEvent(event));
+  // }, [events]);
 
   function handleEventReceive(info) {
     // Add one hour to start time to get end time
@@ -31,8 +33,8 @@ const Calendar = () => {
       end: infoEventEnd,
       id: info.event.id,
       allDay: true,
+      durationEditable: true,
     };
-
     setEvents((prevEvents) => [...prevEvents, newEvent]);
   }
 
@@ -40,17 +42,46 @@ const Calendar = () => {
     let title = prompt("Veuillez entrer le titre de l'événement");
     let calendarApi = selectInfo.view.calendar;
     calendarApi.unselect();
+
     if (title) {
       const newEvent = {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-        id: Date.now(),
+        id: Date.now().toString(),
         allDay: selectInfo.allDay,
       };
-      calendarApi.addEvent(newEvent);
       setEvents((prevEvents) => [...prevEvents, newEvent]);
     }
+  }
+
+  function handleEventResize(info) {
+    console.log(
+      "🚀 ~ file: Calendar.jsx:57 ~ handleEventResize ~ info:",
+      info.event
+    );
+    const endDate = info.event._instance.range.end;
+    const formattedEndDate = `${endDate.getFullYear()}-${(
+      "0" +
+      (endDate.getMonth() + 1)
+    ).slice(-2)}-${("0" + endDate.getDate()).slice(-2)}`;
+
+    console.log("formattedEndDate", formattedEndDate);
+    const resizedEventId = info.event.id;
+
+    setEvents((prevEvents) => {
+      return prevEvents.map((event) => {
+        const eventId = event.id;
+        if (eventId === resizedEventId) {
+          console.log("maj");
+          return {
+            ...event,
+            end: formattedEndDate,
+          };
+        }
+        return event;
+      });
+    });
   }
 
   function handleEventClick(info) {
@@ -78,6 +109,7 @@ const Calendar = () => {
         eventClick={handleEventClick}
         select={handleDateSelect}
         eventReceive={handleEventReceive}
+        eventResize={handleEventResize}
         ref={calendarRef}
         hiddenDays={[0, 6]}
         initialView="dayGridMonth"
