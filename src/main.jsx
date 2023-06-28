3b84a372a02a91294a67062a9f919213d5fc94d6
traducha <traducha@vayandata.com>
2023-06-28 12:13:01 +0200

    updated(main): delete StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,6 @@
 import App from "./App.jsx";
 import "./index.css";
 /* Libs & plugins */
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-//vérifier si strictmode est utile
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
